c654bc66c9acae7ec7d55c47b71b1d7a4d631715
Jayden Major <jaydenmajor@gmail.com>
2016-02-16 16:57:31 +1000

    Create stick-with-me-menu.php

---
+++ stick-with-me-menu/stick-with-me-menu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Stick With Me Menu
+Version: 1.0
+Author: Jayden Major & Jason Ovalle
+Author URI:  https://jaydenmajor.com/plugins
+Description: This plugin makes the add pages section of the wordpress menu editor stay on screeen no matter how long your menu is.
+Tags: Menu editor, menu, moves adding menu, wordpress admin,  Jayden Major & Jason Ovalle, Jayden Major, Jason Ovalle
+Text Domain: stick-with-me-menu
+Licence: GPLv3
+Licence URI: http://www.gnu.org/licenses/gpl.html
+*/
+/*
+* Adding css to admin pages.
+*/
+add_action( 'admin_enqueue_scripts', 'stickwithmemenu_scripts' );
+function stickwithmemenu_scripts(){
+	 wp_enqueue_style( plugin_dir_url( __FILE__ ) . 'assets/stick-with-me-menu.css' );
+}
+?>
