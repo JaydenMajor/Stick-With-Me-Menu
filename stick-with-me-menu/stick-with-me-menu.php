029231931f936907640b826883511f708da3a6ae
Jayden Major <jaydenmajor@gmail.com>
2016-03-03 11:43:17 +1000

    Update stick-with-me-menu.php

--- stick-with-me-menu/stick-with-me-menu.php
+++ stick-with-me-menu/stick-with-me-menu.php
@@ -17,7 +17,7 @@ function stickwithmemenu_style() {
 	$value = get_option('stickwithmemenu_enabled', 'true' );
 	if($value != 'false'){
 		wp_register_style('stickwithmecss',plugin_dir_url( __FILE__ ) .'/assets/stick-with-me-menu.css');
-    	wp_enqueue_style('stickwithmecss');
+    		wp_enqueue_style('stickwithmecss');
 	}
 }
 add_action( 'admin_enqueue_scripts', 'stickwithmemenu_style' );
@@ -25,13 +25,13 @@ add_action( 'admin_enqueue_scripts', 'stickwithmemenu_style' );
 add_action('admin_init','stickwithmemenu_init');
 function stickwithmemenu_init(){
 	register_setting('general','stickwithmemenu_enabled');
-	add_settings_field('stickwithmemenu_enabled_setting','Stick With Me Menu','stickwithmemenu_displayform','general','default',array( 'label_for' => 'stickwithmemenu_enabled' ));
+	add_settings_field('stickwithmemenu_enabled_setting',__('Stick With Me Menu'),'stickwithmemenu_displayform','general','default',array( 'label_for' => 'stickwithmemenu_enabled' ));
 }
 function stickwithmemenu_displayform($args){
 	$value = get_option('stickwithmemenu_enabled', 'true' );
 	?>
-	<label for="<?php echo $args['label_for']; ?>"><input type="radio" id="<?php echo $args['label_for']; ?>" name="<?php echo $args['label_for']; ?>" value="true" <?php if($value != 'false'){echo 'checked';}?>/> Enabled</label>&nbsp;
-	<label for="<?php echo $args['label_for']; ?>_off"><input type="radio" id="<?php echo $args['label_for']; ?>_off" name="<?php echo $args['label_for']; ?>" value="false" <?php if($value == 'false'){echo 'checked';}?>/> Disabled</label>
+	<label for="<?php echo $args['label_for']; ?>"><input type="radio" id="<?php echo $args['label_for']; ?>" name="<?php echo $args['label_for']; ?>" value="true" <?php if($value != 'false'){echo 'checked';}?>/> <?php _e('Enabled','stick-with-me-menu'); ?></label>&nbsp;
+	<label for="<?php echo $args['label_for']; ?>_off"><input type="radio" id="<?php echo $args['label_for']; ?>_off" name="<?php echo $args['label_for']; ?>" value="false" <?php if($value == 'false'){echo 'checked';}?>/> <?php _e('Disabled','stick-with-me-menu'); ?></label>
 	<?php
 }
 ?>
